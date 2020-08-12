1272da9940bc81ec45fe121bb320614748cc5042
Hannah Arnett <hannahtesaker@gmail.com>
2020-08-12 11:45:24 -0700

    Stop empty palettes from being added to db

--- client/src/components/BuildPalette.js
+++ client/src/components/BuildPalette.js
@@ -4,115 +4,129 @@ import ModifyColorBox from "./ModifyColorBox";
 import styles from "../styles/BuildPalette.module.css";
 import ColorfulAPI from "./ColorfulAPI";
 import Button from './Button';
+import Modal from "./Modal";
 
 class BuildPalette extends Component {
-    static defaultProps = {
-        maxColors: 20
+  static defaultProps = {
+    maxColors: 20
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: [],
+      name: "",
+      id: "",
+      emptyPalette: false
     };
-    constructor(props) {
-        super(props);
-        this.state = {
-            colors: [],
-            name: "",
-            id: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.addNewColor = this.addNewColor.bind(this);
-        this.removeColor = this.removeColor.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    componentDidMount() {
-        if(this.props.id) {
-            ColorfulAPI.getOnePalette(this.props.id).then(data => {
-                this.setState({
-                    colors: data.colors,
-                    name: data.name,
-                    id: data._id
-                });
-            });
-        }
-    }
-    handleChange(e) {
-        e.preventDefault();
+    this.handleChange = this.handleChange.bind(this);
+    this.addNewColor = this.addNewColor.bind(this);
+    this.removeColor = this.removeColor.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    if (this.props.id) {
+      ColorfulAPI.getOnePalette(this.props.id).then(data => {
         this.setState({
-          [e.target.name]: e.target.value
+          colors: data.colors,
+          name: data.name,
+          id: data._id
         });
+      });
     }
-    addNewColor(newColor) {
-        this.setState({
-          colors: [...this.state.colors, newColor]
-        });
+  }
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  addNewColor(newColor) {
+    this.setState({
+      colors: [...this.state.colors, newColor]
+    });
+  }
+  removeColor(colorName) {
+    this.setState({
+      colors: this.state.colors.filter(color => color.name !== colorName)
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const data = {
+      name: this.state.name,
+      colors: this.state.colors
+    };
+
+    if (this.state.colors.length === 0) {
+      this.setState({ emptyPalette: true }, () => {
+        setTimeout(() => this.setState({ emptyPalette: false }), 3000);
+      });
+      return;
     }
-    removeColor(colorName) {
-        this.setState({
-          colors: this.state.colors.filter(color => color.name !== colorName)
-        });
+
+    if (!this.state.id) {
+      ColorfulAPI.createPalette(data);
+    } else {
+      ColorfulAPI.updatePalette(this.state.id, data)
     }
-    handleSubmit(e) {
-        e.preventDefault();
-        const data = {
-          name: this.state.name,
-          colors: this.state.colors
-        };
-        
-        if(!this.state.id) {
-            ColorfulAPI.createPalette(data);
-        } else {
-            ColorfulAPI.updatePalette(this.state.id, data)
-        }
-        
-        this.setState({
-          name: "",
-          colors: [],
-          id: ""
-        });
-    }
-    passTitleToChild() {
-        this.props.callbackFromParent(this.state.name);
-    }
-    render() {
-        const { colors = [], name, id } = this.state;
-        const { maxColors } = this.props;
-        const fullPalette = colors.length >= maxColors;
-        if(id) {this.passTitleToChild()}
-        return (
-            <div className={styles.container}>
-            <div className={styles.colorPickerContainer}>
-              <ColorPickerForm
-                addNewColor={this.addNewColor}
-                fullPalette={fullPalette}
-                colors={colors}
-              />
-              <div className={styles.formContainer}>
-                <form onSubmit={this.handleSubmit}>
-                  <div className={styles.submit}>
-                    <input
-                      type="text"
-                      value={name}
-                      name="name"
-                      onChange={this.handleChange}
-                      required
-                      autoComplete="off"
-                      placeholder="Palette name"
-                    />
-                    <Button text="Save" filled />
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div className={styles.colors}>
-              {colors.map(color => (
-                <ModifyColorBox
-                  background={color.color}
-                  name={color.name}
-                  onClick={this.removeColor}
-                  key={color.name}
+
+    this.setState({
+      name: "",
+      colors: [],
+      id: ""
+    });
+  }
+  passTitleToChild() {
+    this.props.callbackFromParent(this.state.name);
+  }
+  render() {
+    const { colors = [], name, id, emptyPalette } = this.state;
+    const { maxColors } = this.props;
+    const fullPalette = colors.length >= maxColors;
+    if (id) { this.passTitleToChild() }
+    return (
+      <div className={styles.container}>
+        <div className={styles.colorPickerContainer}>
+          {emptyPalette ? (
+            <Modal onModalClose={() => this.setState({ emptyPalette: false })}>
+              <h1>Palette is empty!</h1>
+            </Modal>
+          ) : null}
+          <ColorPickerForm
+            addNewColor={this.addNewColor}
+            fullPalette={fullPalette}
+            colors={colors}
+          />
+          <div className={styles.formContainer}>
+            <form onSubmit={this.handleSubmit}>
+              <div className={styles.submit}>
+                <input
+                  type="text"
+                  value={name}
+                  name="name"
+                  onChange={this.handleChange}
+                  required
+                  autoComplete="off"
+                  placeholder="Palette name"
                 />
-              ))}
-            </div>
-          </div> 
-        );
-    }
+                <Button text="Save" filled />
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className={styles.colors}>
+          {colors.map(color => (
+            <ModifyColorBox
+              background={color.color}
+              name={color.name}
+              onClick={this.removeColor}
+              key={color.name}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default BuildPalette;
